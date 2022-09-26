32f8062fb2d842c192a31b39daac50a333bf0dd9
bantawa04 <rahunn3@gmail.com>
2022-09-26 12:12:00 +0545

    Bottom navigation tab added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -26,10 +23,36 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: _CustomAppBar(),
+        appBar: const _CustomAppBar(),
+        bottomNavigationBar: const _CustomNavBar(),
         body: Container(),
       ),
     );
+  }
+}
+
+class _CustomNavBar extends StatelessWidget {
+  const _CustomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.deepPurple.shade800,
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Colors.white,
+      showUnselectedLabels: false,
+      showSelectedLabels: false,
+      items: const [
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.play_circle_fill_outlined),
+          label: "Now Playing"),
+      BottomNavigationBarItem(icon: Icon(Icons.favorite_border_outlined), label: "Favourites"),
+      BottomNavigationBarItem(icon: Icon(Icons.people_outline), label: "Profile"),
+    ]);
   }
 }
 
@@ -48,14 +71,14 @@ class _CustomAppBar extends StatelessWidget with PreferredSizeWidget {
         Container(
           margin: const EdgeInsets.only(right: 20),
           child: const CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://randomuser.me/api/portraits/women/17.jpg"),
+            backgroundImage:
+                NetworkImage("https://randomuser.me/api/portraits/men/1.jpg"),
           ),
         )
       ],
     );
   }
-  
+
   @override
   // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(56.0);
