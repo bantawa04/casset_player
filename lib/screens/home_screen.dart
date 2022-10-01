5c75f3a309beb15d71c0f6b88ef9b4eb121d2386
bantawa04 <rahunn3@gmail.com>
2022-10-01 12:12:00 +0545

    Section header widget created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:casset_player/models/song_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Song> songs = Song.songs;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,12 +29,40 @@ class _HomeScreenState extends State<HomeScreen> {
         bottomNavigationBar: const _CustomNavBar(),
         body: SingleChildScrollView(
           child: Column(
-            children: const <Widget>[
-              DiscoverMusic(),
+            children: <Widget>[
+              const DiscoverMusic(),
+              Column(
+                children: const <Widget>[
+                  SectionHeader()
+                ],
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class SectionHeader extends StatelessWidget {
+  const SectionHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          "Trending music",
+          style: Theme.of(context)
+              .textTheme
+              .headline6!
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
+        const Text("View more")
+      ],
     );
   }
 }
